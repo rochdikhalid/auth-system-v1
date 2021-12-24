4ba956d28988a02e8b2e54e2fdff286773952004
rochdikhalid <rochdib.khalid@gmail.com>
2021-12-24 20:01:57 +0100

    Update tokens.py

--- auth/users/tokens.py
+++ auth/users/tokens.py
@@ -4,21 +4,17 @@ from django.utils import six
 
 
 class TokenGenerator(PasswordResetTokenGenerator):
-
     # To hash user's primary key and user state for producing a token that is invalidated when it's used
     def _make_hash_value(self, user, timestamp):
         return (
             six.text_type(user.pk) + six.text_type(timestamp) +
             six.text_type(user.is_active)
-        )
-        
+        )      
 account_activation_token = TokenGenerator()
 
-class ResetRequestTokenGenerator(PasswordResetTokenGenerator):
-    
+class ResetRequestTokenGenerator(PasswordResetTokenGenerator):   
     def _make_hash_value(self, user, timestamp):
         return (
             six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.password) + six.text_type(user.times_password_changed) 
         )
-
 reset_password_token = ResetRequestTokenGenerator()
